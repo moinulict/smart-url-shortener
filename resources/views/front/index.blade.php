8f6673d58aebfab26b18ac6b739dd8c1a6868ac7
moinulict <engrminul@gmail.com>
2023-08-25 22:53:17 +0600

    css modified

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -6,11 +6,12 @@
   @stop
   @section('contents')
       <!-- ======= Hero Section ======= -->
-      <section id="hero" class="hero d-flex align-items-center">
+      <section class="d-flex align-items-center">
           <div class="container">
-              <div class="row gy-4 d-flex justify-content-between">
-                  <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                      <h2 data-aos="fade-up">Free shorten URL generator</h2>
+              <div class="row gy-4">
+                  <div class="col-lg-6">
+                    <div class="genBox">  
+                    <h2 data-aos="fade-up">Free shorten URL generator</h2>
                       <p data-aos="fade-up" data-aos-delay="100">
                         Introducing URLGEN, your go-to solution for hassle-free link sharing. With URLGEN, you can instantly 
                         generate lengthy web URL into concise and shorten links. 
@@ -18,51 +19,18 @@
                       </p>
 
                       <div class="alert alert-danger none errorMsg"></div>
-                      <form action="" id="urlGenForm" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up"
+                      <form action="" id="urlGenForm" class="form-search  mb-3" data-aos="fade-up"
                           data-aos-delay="200">
-                          <input type="text" class="form-control longUrl" placeholder="Please enter your long link here">
-                          <button type="submit" class="btn btn-primary">Generate</button>
+                          <div class="my-2">
+                          <input type="text" class="form-control longUrl" placeholder="Enter your long link here">
+                          </div>
+                          <button type="submit" class="btn btn-primary btn-lg w-100">Generate Shorten URL</button>
                       </form>
 
                       <div class="urlGenResponse none pr"></div>
 
-                      {{-- <div class="row gy-4" data-aos="fade-up" data-aos-delay="400">
-
-                          <div class="col-lg-3 col-6">
-                              <div class="stats-item text-center w-100 h-100">
-                                  <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1"
-                                      class="purecounter"></span>
-                                  <p>Clients</p>
-                              </div>
-                          </div><!-- End Stats Item -->
-
-                          <div class="col-lg-3 col-6">
-                              <div class="stats-item text-center w-100 h-100">
-                                  <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1"
-                                      class="purecounter"></span>
-                                  <p>Projects</p>
-                              </div>
-                          </div><!-- End Stats Item -->
-
-                          <div class="col-lg-3 col-6">
-                              <div class="stats-item text-center w-100 h-100">
-                                  <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1"
-                                      class="purecounter"></span>
-                                  <p>Support</p>
-                              </div>
-                          </div><!-- End Stats Item -->
-
-                          <div class="col-lg-3 col-6">
-                              <div class="stats-item text-center w-100 h-100">
-                                  <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1"
-                                      class="purecounter"></span>
-                                  <p>Workers</p>
-                              </div>
-                          </div><!-- End Stats Item -->
-
-                      </div> --}}
                   </div>
-
+                </div>
               </div>
           </div>
       </section><!-- End Hero Section -->
