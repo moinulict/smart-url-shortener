57f2574c0d0bfc706c32bef93edb2b68de706ac7
minulsit <minul@sundarbanit.com.bd>
2023-08-29 17:25:14 +0600

    last

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -6,6 +6,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="baseUrl" content="{{url('/')}}">
+    <link rel="canonical" href="https://www.urlgen.io/">
     @yield('meta')
 
     <!-- Favicons -->
